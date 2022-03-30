a2b5744f56253b09c0f3027073e7db667ef62dd5
sltnsrh <lifenjoy8888@gmail.com>
2022-03-30 13:42:50 +0300

    deleted redundant exception from AuthenticationFilter

--- src/main/java/taxi/web/filter/AuthenticationFilter.java
+++ src/main/java/taxi/web/filter/AuthenticationFilter.java
@@ -17,7 +17,7 @@ public class AuthenticationFilter implements Filter {
     private final Set<String> allowedUrls = new HashSet<>();
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
         allowedUrls.add("/login");
         allowedUrls.add("/drivers/add");
     }
